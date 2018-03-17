bb3ed5509f7d1401ae2061dd2b50cb15c8fe1d3f
floco <info@mutualis.com>
2018-03-17 14:13:53 +0100

    correct typo

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Personal beancount importers",
     author="jamatute",
     author_email="jmm@riseup.net",
-    url="https://github.com/floco/beancount-importers",
+    url="https://github.com/floco/beancount-importer",
     license='GPLv2',
     packages=['beancount_importers'],
     long_description=open('README.md').read(),
