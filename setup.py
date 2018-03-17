3154eb947c8c7696e2b3508363723d415d2edb96
floco <info@mutualis.com>
2018-03-17 14:07:58 +0100

    updated url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Personal beancount importers",
     author="jamatute",
     author_email="jmm@riseup.net",
-    url="https://github.com/jamatute/beancount-importers",
+    url="https://github.com/floco/beancount-importers",
     license='GPLv2',
     packages=['beancount_importers'],
     long_description=open('README.md').read(),
